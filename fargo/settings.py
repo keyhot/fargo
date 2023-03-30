32bbbcdd787eef3d0e756d2bc29b67c641d0b2dc
Yryskeldi Azamatov <azamatov0202@gmail.com>
2023-03-30 13:47:13 +0200

    changed the phone number and some details

--- fargo/settings.py
+++ fargo/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -88,7 +88,7 @@ DATABASES = {
         'HOST': os.environ.get('PSQL_DB_HOST'),
         'PORT': '5432',
     }
-#
+
 # 'default': {
 #         'ENGINE': 'django.db.backends.sqlite3',
 #         'NAME': 'db.sqlite3',
