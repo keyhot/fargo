3563e0a94466c9f4f4aded9cfb226015fb428a9d
Yryskeldi Azamatov <azamatov0202@gmail.com>
2023-04-17 03:01:26 +0200

    made some changes in middleware

--- fargo/middleware.py
+++ fargo/middleware.py
@@ -10,6 +10,10 @@ class HttpsMiddleware:
             url = request.build_absolute_uri(request.get_full_path())
             if url.startswith("http://www."):
                 secure_url = url.replace("http://www.", "https://www.")
+            elif url.startswith("https://fargo"):
+                secure_url = url.replace("https://", "https://www.")
+            elif url.startswith("http://fargo"):
+                secure_url = url.replace("http://", "https://www.")
             else:
                 secure_url = url.replace("http://", "https://")
             return HttpResponsePermanentRedirect(secure_url)
