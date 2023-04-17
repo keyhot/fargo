0ab0a69448576fafcbdba383ed8ac96c52f79724
Yryskeldi Azamatov <azamatov0202@gmail.com>
2023-04-17 02:44:31 +0200

    Added middleware that redirects to secure page

---
+++ fargo/middleware.py
@@ -0,0 +1,17 @@
+from django.http import HttpResponsePermanentRedirect
+
+
+class HttpsMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        if not request.is_secure():
+            url = request.build_absolute_uri(request.get_full_path())
+            if url.startswith("http://www."):
+                secure_url = url.replace("http://www.", "https://www.")
+            else:
+                secure_url = url.replace("http://", "https://")
+            return HttpResponsePermanentRedirect(secure_url)
+        response = self.get_response(request)
+        return response
